f730ff31d5e6e345d238d3ad426fabc06f608fb7
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-09-12 18:24:42 -0500

    Updates

--- src/main/java/com/whd/users/UsersController.java
+++ src/main/java/com/whd/users/UsersController.java
@@ -1,7 +1,21 @@
 package com.whd.users;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsersController {
+
+    private final UsersRepository usersDao;
+
+    public UsersController(UsersRepository usersDao) {
+        this.usersDao = usersDao;
+    }
+
+    @GetMapping("/admin-dashboard")
+    public String clientList(Model model) {
+        model.addAttribute("users", usersDao.findAll());
+        return "admin-dashboard/users";
+    }
 }
